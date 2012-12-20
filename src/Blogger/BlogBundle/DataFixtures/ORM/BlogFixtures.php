e3a5b65dd3ef2229bfcb2c2bddcac42ab4f0c839
Bogdan <bbuhai@pentalog.fr>
2012-12-20 10:27:19 +0200

    Initial commit test

--- src/Blogger/BlogBundle/DataFixtures/ORM/BlogFixtures.php
+++ src/Blogger/BlogBundle/DataFixtures/ORM/BlogFixtures.php
@@ -12,7 +12,7 @@ class BlogFixtures implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         $blog1 = new Blog();
-        $blog1->setTitle('A day with Symfony2');
+        $blog1->setTitle('A day with Symfony2.14');
         $blog1->setBlog('Lorem ipsum dolor sit amet, consectetur adipiscing eletra electrify denim vel ports.\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut velocity magna. Etiam vehicula nunc non leo hendrerit commodo. Vestibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur tempus nisl vitae viverra. Cras el mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur tempus nisl vitae viverra. Cras elementum molestie vestibulum. Morbi id quam nisl. Praesent hendrerit, orci sed elementum lobortis, justo mauris lacinia libero, non facilisis purus ipsum non mi. Aliquam sollicitudin, augue id vestibulum iaculis, sem lectus convallis nunc, vel scelerisque lorem tortor ac nunc. Donec pharetra eleifend enim vel porta.');
         $blog1->setImage('beach.jpg');
         $blog1->setAuthor('dsyph3r');
