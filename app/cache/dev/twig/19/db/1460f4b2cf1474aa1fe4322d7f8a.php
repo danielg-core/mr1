5e91da00113f7bc1e4cd3e8ef1df4c9864cb2969
thornai <thornai@pentalog.fr>
2012-12-20 09:56:50 +0200

    - add sources

---
+++ app/cache/dev/twig/19/db/1460f4b2cf1474aa1fe4322d7f8a.php
@@ -0,0 +1,83 @@
+<?php
+
+/* WebProfilerBundle:Profiler:toolbar_js.html.twig */
+class __TwigTemplate_19db1460f4b2cf1474aa1fe4322d7f8a extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<div id=\"sfwdt";
+        echo twig_escape_filter($this->env, $this->getContext($context, "token"), "html", null, true);
+        echo "\" class=\"sf-toolbar\" style=\"display: none\"></div>
+";
+        // line 2
+        $this->env->loadTemplate("WebProfilerBundle:Profiler:base_js.html.twig")->display($context);
+        // line 3
+        echo "<script type=\"text/javascript\">/*<![CDATA[*/
+    (function () {
+        ";
+        // line 5
+        if (("top" == $this->getContext($context, "position"))) {
+            // line 6
+            echo "            var sfwdt = document.getElementById('sfwdt";
+            echo twig_escape_filter($this->env, $this->getContext($context, "token"), "html", null, true);
+            echo "');
+            document.body.insertBefore(
+                document.body.removeChild(sfwdt),
+                document.body.firstChild
+            );
+        ";
+        }
+        // line 12
+        echo "
+        Sfjs.load(
+            'sfwdt";
+        // line 14
+        echo twig_escape_filter($this->env, $this->getContext($context, "token"), "html", null, true);
+        echo "',
+            '";
+        // line 15
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_wdt", array("token" => $this->getContext($context, "token"))), "html", null, true);
+        echo "',
+            function(xhr, el) {
+                el.style.display = -1 !== xhr.responseText.indexOf('sf-toolbarreset') ? 'block' : 'none';
+            },
+            function(xhr) {
+                if (xhr.status !== 0) {
+                    confirm('An error occurred while loading the web debug toolbar (' + xhr.status + ': ' + xhr.statusText + ').\\n\\nDo you want to open the profiler?') && (window.location = '";
+        // line 21
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_profiler", array("token" => $this->getContext($context, "token"))), "html", null, true);
+        echo "');
+                }
+            }
+        );
+    })();
+/*]]>*/</script>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "WebProfilerBundle:Profiler:toolbar_js.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  59 => 21,  50 => 15,  30 => 5,  26 => 3,  24 => 2,  19 => 1,  176 => 52,  171 => 47,  168 => 46,  163 => 42,  158 => 39,  150 => 34,  142 => 33,  134 => 26,  130 => 25,  126 => 24,  122 => 22,  119 => 21,  113 => 12,  109 => 10,  106 => 9,  100 => 5,  95 => 53,  93 => 52,  88 => 49,  86 => 46,  81 => 43,  79 => 42,  75 => 40,  73 => 39,  65 => 34,  61 => 33,  56 => 30,  54 => 21,  41 => 9,  34 => 5,  28 => 1,  46 => 14,  43 => 14,  37 => 6,  32 => 6,  60 => 14,  53 => 12,  48 => 10,  42 => 12,  38 => 7,  35 => 6,  29 => 4,);
+    }
+}
---
+++ app/cache/dev/twig/6e/5b/b34a1666ec64a13282df05d7aa2d.php
@@ -0,0 +1,103 @@
+<?php
+
+/* WebProfilerBundle:Profiler:base_js.html.twig */
+class __TwigTemplate_6e5bb34a1666ec64a13282df05d7aa2d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<script type=\"text/javascript\">/*<![CDATA[*/
+    Sfjs = (function() {
+        \"use strict\";
+
+        var noop = function() {},
+            request = function(url, onSuccess, onError, payload, options) {
+                var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+                options = options || {};
+                xhr.open(options.method || 'GET', url, true);
+                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                xhr.onreadystatechange = function(state) {
+                    if (4 === xhr.readyState && 200 === xhr.status) {
+                        (onSuccess || noop)(xhr);
+                    } else if (4 === xhr.readyState && xhr.status != 200) {
+                        (onError || noop)(xhr);
+                    }
+                };
+                xhr.send(payload || '');
+            },
+            hasClass = function(el, klass) {
+                return el.className.match(new RegExp('\\\\b' + klass + '\\\\b'));
+            },
+            removeClass = function(el, klass) {
+                el.className = el.className.replace(new RegExp('\\\\b' + klass + '\\\\b'), ' ');
+            },
+            addClass = function(el, klass) {
+                if (!hasClass(el, klass)) { el.className += \" \" + klass; }
+            };
+
+        return {
+            hasClass: hasClass,
+            removeClass: removeClass,
+            addClass: addClass,
+            request: request,
+            load: function(selector, url, onSuccess, onError, options) {
+                var el = document.getElementById(selector);
+
+                if (el && el.getAttribute('data-sfurl') !== url) {
+                    request(
+                        url,
+                        function(xhr) {
+                            el.innerHTML = xhr.responseText;
+                            el.setAttribute('data-sfurl', url);
+                            removeClass(el, 'loading');
+                            (onSuccess || noop)(xhr, el);
+                        },
+                        function(xhr) { (onError || noop)(xhr, el); },
+                        options
+                    );
+                }
+
+                return this;
+            },
+            toggle: function(selector, elOn, elOff) {
+                var i,
+                    style,
+                    tmp = elOn.style.display,
+                    el = document.getElementById(selector);
+
+                elOn.style.display = elOff.style.display;
+                elOff.style.display = tmp;
+
+                if (el) {
+                    el.style.display = 'none' === tmp ? 'none' : 'block';
+                }
+
+                return this;
+            }
+
+        }
+    })();
+/*]]>*/</script>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "WebProfilerBundle:Profiler:base_js.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  59 => 21,  50 => 15,  30 => 5,  26 => 3,  24 => 2,  19 => 1,  176 => 52,  171 => 47,  168 => 46,  163 => 42,  158 => 39,  150 => 34,  142 => 33,  134 => 26,  130 => 25,  126 => 24,  122 => 22,  119 => 21,  113 => 12,  109 => 10,  106 => 9,  100 => 5,  95 => 53,  93 => 52,  88 => 49,  86 => 46,  81 => 43,  79 => 42,  75 => 40,  73 => 39,  65 => 34,  61 => 33,  56 => 30,  54 => 21,  41 => 9,  34 => 5,  28 => 1,  46 => 14,  43 => 14,  37 => 6,  32 => 6,  60 => 14,  53 => 12,  48 => 10,  42 => 12,  38 => 7,  35 => 6,  29 => 4,);
+    }
+}
